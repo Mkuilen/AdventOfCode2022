5e0b90ff9e6751762c4bd327cc1f562b4de39753
mkuilen <mvdkuilen@kpnmail.nl>
2022-12-01 10:34:55 +0100

    Kom er niet uit

--- Day1/Puzzle1.kt
+++ Day1/Puzzle1.kt
@@ -1,6 +1,12 @@
-fun main(){
-    val inputStream: InputStream = File("../Input/Day1/puzzle1.txt").inputStream();
+package Day1;
+
+import java.io.File
+import java.io.InputStream
+
+fun main(args: Array<String>){
+    val inputStream: InputStream = File("Input/Day1/puzzle1.txt").inputStream();
     val inputString = inputStream.bufferedReader().use { it.readText() }
 
-    println(inputString)
+
+
 }
